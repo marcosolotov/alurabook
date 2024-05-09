35c9a9bd26509fd1275e32449d6efe95025a9713
marcosolotov <marcoaureliosolotov@outlook.com>
2024-05-09 18:49:56 -0300

    Primeiro commit.

---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,52 @@
+import { ConsultaCepService } from './../service/consulta-cep.service';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent implements OnInit {
+
+  constructor(
+    private router: Router,
+    private ConsultaCepService: ConsultaCepService) { }
+
+  ngOnInit(): void {
+  }
+
+  cadastrar(form : NgForm){
+    if (form.valid) {
+      this.router.navigate(['./sucesso'])
+    } else {
+      alert('Formulário inválido!')
+    }
+      console.log('Formulário enviado');
+  }
+
+   
+
+  consultaCep(ev:any, f: NgForm) {
+    const cep = ev.target.value;
+    if(cep !== ""){
+        this.ConsultaCepService.getConsultaCep(cep).subscribe((resultado) => 
+        {
+          console.log(resultado);
+          this.populandoEndereco(resultado, f);
+        });
+    }
+  } 
+
+  populandoEndereco(dados: any, f: NgForm) {
+    f.form.patchValue({
+      endereco: dados.logradouro,
+      complemento: dados.complemento,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      estado: dados.uf
+    })
+  }
+
+}
